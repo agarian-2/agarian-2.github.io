b7ebedc21f351e7de0a3b664a00476e85acfb404
Tom <36286279+agarian-2@users.noreply.github.com>
2024-06-21 21:53:29 -0400

    Skins list test 400

--- include/gallery.php
+++ include/gallery.php
@@ -9,9 +9,14 @@
             $skindirhtml = "./skins/";
 
             $images = scandir($skindir);
-
+            function debug_to_console($data) {
+                $output = $data;
+                if (is_array($output)) $output = implode(',', $output);
+                echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+            }
             foreach($images as $curimg) {
                 if (strtolower(pathinfo($curimg, PATHINFO_EXTENSION)) == "png") {
+                    debug_to_console("./", $skindirhtml.$curimg);
         ?>
         <li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
             <div class="circular" style='background-image: url(<?php echo "./", $skindirhtml.$curimg ?>)'></div>
